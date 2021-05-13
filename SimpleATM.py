1eccdf00f33213928f024c331fe50cb22c6cce09
hmoon14 <36491985+hmoon14@users.noreply.github.com>
2021-05-13 00:43:00 -0700

    Add files via upload
    Not completed yet

---
+++ SimpleATM.py
@@ -0,0 +1,44 @@
+menu_select =0
+class BankAccount:
+    global menu_select
+    def __init__(self):
+        self.AN =000000
+        self.PIN = 0000
+        self.current =0
+    def update_account(self, new_pin):
+        self.account = account
+    def update_pin(self, new_pin):
+        self.PIN = new_pin
+    def update_balance(self, balance):
+        if menu_selct == 1
+            self.current -= balance
+        elif menu_selct == 2
+            self.current += balance
+        
+        
+
+acc = input("Input 5 digit account number")
+Pin = input("Input 4 digit pin")
+# this would be replaced with real card reading in the ATM
+
+print("""Welcome to Test ATM
+1. Withdraw
+2. Deposit
+3. Check Balance
+4. Open Account""")
+
+menu_select = input()
+
+if menu_select == 1:
+    amount = input("How much would you like to withdraw")
+    
+
+elif menu_select == 2:
+
+elif menu_select ==3:
+
+elif menu_select == 4:
+
+else:
+    print("There is no button other than 1,2,3,4 in the UI")
+    
